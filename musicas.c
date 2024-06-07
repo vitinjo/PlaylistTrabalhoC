76f69f7e95cb8bc3c70b7238bfdf76e31d071c71
vitinjo <135727142+vitinjo@users.noreply.github.com>
2024-06-06 23:36:43 -0300

    Update musicas.c
    Verifica se a lista está vazia (ou seja, se head é NULL). Se estiver vazia, imprime uma mensagem informando que a playlist está vazia e retorna da função.
    Após isso Abre o arquivo especificado pelo nome fileName no modo de leitura ("r").
    Se o arquivo não puder ser aberto, imprime uma mensagem de erro e retorna da função.

--- musicas.c
+++ musicas.c
@@ -25,3 +25,32 @@ void append(CircularDoublyLinkedlista *lista, char *artista, char *musica) {
     lista->head->prev = newNode;
   }
 }
+
+void display(CircularDoublyLinkedlista *lista) {
+  if (!lista->head) {
+    printf("Playlist vazia.\n");
+    return;
+  }
+  Node *atual = lista->head;
+  do {
+    printf("%s - %s\n", atual->artista, atual->musica);
+    atual = atual->next;
+  } while (atual != lista->head);
+}
+
+void readFile(CircularDoublyLinkedlista *lista, const char *fileName) {
+  FILE *file = fopen(fileName, "r");
+  if (!file) {
+    perror("Não foi possível abrir");
+    return;
+  }
+  char line[256];
+  while (fgets(line, sizeof(line), file)) {
+    char *artista = strtok(line, ";");
+    char *musica = strtok(NULL, "\n");
+    if (artista && musica) {
+      append(lista, artista, musica);
+    }
+  }
+  fclose(file);
+}
